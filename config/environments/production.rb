58b89181cca9ed1c244755a9e9f592f4b4363e8e
nicolas-m-rey <nic.m.rey@gmail.com>
2023-05-24 14:52:23 -0700

    fixed spelling error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = falsek
+  config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
 
