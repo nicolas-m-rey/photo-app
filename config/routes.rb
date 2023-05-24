59c2a730b485d51f712a807342e86caf9447442d
nicolas-m-rey <nic.m.rey@gmail.com>
2023-05-24 15:04:16 -0700

    registrations controller for card payment verifications

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :images
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
