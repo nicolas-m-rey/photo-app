93f622883113684c484cf84c142f289361ae0e8c
nicolas-m-rey <nic.m.rey@gmail.com>
2023-05-25 11:01:34 -0700

    configured google mailer with env variables

--- config/environment.rb
+++ config/environment.rb
@@ -4,22 +4,22 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-# config.action_mailer.delivery_method = :smtp   host = "[your app's name].herokuapp.com"   
-# 
-# config.action_mailer.default_url_options = { host: host, protocol: 'https' }   
-# 
-# ActionMailer::Base.smtp_settings = {     
-# 
-# :address => 'smtp.gmail.com',
-# 
-# :port => '587',
-# 
-# :authentication => :plain,
-# 
-# :user_name => ENV['GMAIL_USERNAME'],     
-# 
-# :password => ENV['GMAIL_PASSWORD'],     
-# 
-# :domain => 'gmail.com',   
-# 
-# :enable_starttls_auto => true }
+config.action_mailer.delivery_method = :smtp   host = "https://photo-app-rails-7.herokuapp.com/~/."   
+
+config.action_mailer.default_url_options = { host: host, protocol: 'https' }   
+
+ActionMailer::Base.smtp_settings = {     
+
+:address => 'smtp.gmail.com',
+
+:port => '587',
+
+:authentication => :plain,
+
+:user_name => ENV['GMAIL_USERNAME'],     
+
+:password => ENV['GMAIL_PASSWORD'],     
+
+:domain => 'gmail.com',   
+
+:enable_starttls_auto => true }
