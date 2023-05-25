8c1f5eb65e5ad6be6c7fb734c6d71c401bf59f6a
nicolas-m-rey <nic.m.rey@gmail.com>
2023-05-25 11:05:02 -0700

    fixing unexpected end of input on mailer environment

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.action_mailer.delivery_method = :smtp   host = "https://photo-app-rails-7.herokuapp.com/~/."   
+config.action_mailer.delivery_method = { :smtp   host = "https://photo-app-rails-7.herokuapp.com/" } 
 
 config.action_mailer.default_url_options = { host: host, protocol: 'https' }   
 
