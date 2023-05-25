dd61a69b3e54f7dcab88ec439830b42478e4a186
nicolas-m-rey <nic.m.rey@gmail.com>
2023-05-25 10:50:01 -0700

    Suppressed logger output for asset requests

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # Suppress logger output for asset requests.
+  config.active_record.sqlite3_production_warning = false
+
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.default_url_options = { host: 
