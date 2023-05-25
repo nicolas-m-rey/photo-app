f8626b9cf76d8e8e15d710e8955e135c8a7d1699
nicolas-m-rey <nic.m.rey@gmail.com>
2023-05-25 11:12:33 -0700

    3rd attempt at debugging environment.rb for mailer

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.action_mailer.delivery_method = :smtp 
+# config.action_mailer.delivery_method = :smtp 
 
 config.action_mailer.default_url_options = { host: host, protocol: 'https' }   
 
