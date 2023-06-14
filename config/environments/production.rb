8dbfc51379ef1eac164973277cbe9f34072a7a59
nicolas-m-rey <nic.m.rey@gmail.com>
2023-06-14 12:46:37 -0700

    missed rename in smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
     :port                 => mailertogo_port,
     :user_name            => mailertogo_user,
     :password             => mailertogo_password,
-    :domain               => mailertogo_url,
+    :domain               => mailertogo_domain,
     :authentication       => :plain,
     :enable_starttls_auto => true,
   }
