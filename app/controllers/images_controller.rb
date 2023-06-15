88e37a014b6cef21961740cf7066e17626e0c6b4
nicolas-m-rey <nic.m.rey@gmail.com>
2023-06-15 12:47:14 -0700

    debugging images/index

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -4,7 +4,7 @@ class ImagesController < ApplicationController
   # GET /images or /images.json
   def index
     @images = Image.all
-    @user = current_user.id
+    @image.user_id = current_user.id
   end
 
   # GET /images/1 or /images/1.json
